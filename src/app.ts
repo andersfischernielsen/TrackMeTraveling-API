724929c15e11883b713485f1f273807498185ee4
Anders Fischer-Nielsen <andersfischern@me.com>
2017-10-26 17:10:15 +0200

    Added debug config, fixed return, added db config.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as Sequelize from "sequelize";
+import { config } from "./database";
 
 const app = express();
 const sequelize = initSequelize();
@@ -12,15 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 verifySequelize(sequelize);
 
 //Routes
-app.get("/api", (req, res) => res.send("API running."));
-app.post("/api/coordinates", (req, res) => res.send(saveCoordinates(req.body.latitude, req.body.longitude)));
+app.get("/", (req, res) => res.send("API running."));
+app.post("/coordinates", async (req, res) => {
+    let result = await saveCoordinates(req.body.username, req.body.latitude, req.body.longitude);
+    res.send(result === true ? 200 : 500);
+}); 
 
 //Functions
-function initSequelize() : Sequelize.Sequelize{
-    return new Sequelize('postgres', 'postgres', 'postgres', {
-        host: 'localhost',
-        dialect: 'postgres',
-        pool: { max: 5, min: 0, idle: 10000 },
+function initSequelize() : Sequelize.Sequelize {
+    return new Sequelize(config.database, config.username, config.password, {
+        host: 'localhost', dialect: 'postgres', pool: { max: 5, min: 0, idle: 10000 },
     });
 }
 
@@ -28,24 +30,25 @@ async function verifySequelize(sequelize: Sequelize.Sequelize) {
     try {
         await sequelize.authenticate();
     } catch (e) {
-        console.error('[Sequelize] Unable to connect to the database:', e);
+        console.error('[Sequelize] Unable to connect to the database: ', e);
     }
 }
 
-async function saveCoordinates(latitude: number, longitude: number): Promise<any> {
+async function saveCoordinates(username: string, latitude: number, longitude: number): Promise<boolean> {
     try {
-        let Coordinates = sequelize.define('coordinate', {
-            id: { type: Sequelize.UUIDV4, primaryKey: true, defaultValue: Sequelize.UUIDV4 },
+        let coordinates = sequelize.define('coordinate', {
+            id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4 },
             username: Sequelize.STRING,
             latitude: Sequelize.DOUBLE,
             longitude: Sequelize.DOUBLE
         });
-        await Coordinates.sync();
-        var created = await Coordinates.create({latitude: latitude, longitude: longitude});
-        return created;
+        await coordinates.sync();
+        var created = await coordinates.create({username: username, latitude: latitude, longitude: longitude});
+        return true;
     } 
     catch(e) {
-        console.log(e);
+        console.error("[Sequelize] Could not save coordinates: ", e);
+        return false;
     }
 }
 
---
+++ src/database.ts
@@ -0,0 +1,7 @@
+let config = {
+    database: "postgres",
+    username: "postgres",
+    password: "postgres",
+}
+
+export { config }
