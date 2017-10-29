4d58505711f863b5dc8e7052c4f66f5687c98139
Anders Fischer-Nielsen <andersfischern@me.com>
2017-10-29 16:16:49 +0100

    Refactored database logic out to separate module.

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,24 @@
+'use strict';
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as Sequelize from "sequelize";
-import { config } from "./database";
+import * as database from "./database";
+import { initialise } from "./database";
 
 const app = express();
-const sequelize = initSequelize();
+var initialised = database.initialise();
 
-//App setup
-app.set("port", process.env.PORT || 3000);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-verifySequelize(sequelize);
+if (app !== undefined && initialised) {
+    app.set("port", process.env.PORT || 3000);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-//Routes
-app.get("/", (req, res) => res.send("API running."));
-app.post("/coordinates", async (req, res) => {
-    let result = await saveCoordinates(req.body.username, req.body.latitude, req.body.longitude);
-    res.send(result === true ? 200 : 500);
-}); 
+    //Routes
+    app.get("/", (req, res) => res.send("API running."));
+    app.post("/coordinates", async (req, res) => {
+        let result = await database.saveCoordinates(req.body.username, req.body.latitude, req.body.longitude);
+        res.send(result === true ? 200 : 500);
+    }); 
 
-//Functions
-function initSequelize() : Sequelize.Sequelize {
-    return new Sequelize(config.database, config.username, config.password, {
-        host: 'localhost', dialect: 'postgres', pool: { max: 5, min: 0, idle: 10000 },
-    });
+    app.listen(5000, 'localhost', () => console.info("API running."));
 }
-
-async function verifySequelize(sequelize: Sequelize.Sequelize) {
-    try { await sequelize.authenticate(); } 
-    catch (e) { console.error('[Sequelize] Unable to connect to the database: ', e); }
-}
-
-async function saveCoordinates(username: string, latitude: number, longitude: number): Promise<boolean> {
-    try {
-        let coordinates = sequelize.define('coordinate', {
-            id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4 },
-            username: { type: Sequelize.STRING, allowNull: false },
-            latitude: Sequelize.DOUBLE,
-            longitude: Sequelize.DOUBLE
-        });
-        await coordinates.sync();
-        var created = await coordinates.create({username: username, latitude: latitude, longitude: longitude});
-        return true;
-    } 
-    catch(e) {
-        console.error("[Sequelize] Could not save coordinates: ", e);
-        return false;
-    }
-}
-
-//Init
-app.listen(5000, 'localhost', () => console.info("API running."));
--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,34 @@
-let config = {
-    database: "postgres",
-    username: "postgres",
-    password: "postgres",
+import * as Sequelize from "sequelize";
+import { config } from "./databaseConfig";
+
+var instance : Sequelize.Sequelize;
+
+function initSequelize() : Sequelize.Sequelize {
+    return new Sequelize(config.database, config.username, config.password, {
+        host: 'localhost', dialect: 'postgres', pool: { max: 5, min: 0, idle: 10000 },
+    });
 }
 
-export { config }
+async function verifySequelize(sequelize: Sequelize.Sequelize) {
+    try { await sequelize.authenticate(); return true; } 
+    catch (e) { console.error('[Sequelize] Unable to connect to the database: ', e); return false; }
+}
+
+async function saveCoordinates(username: string, latitude: number, longitude: number) {
+    let coordinates = instance.define('coordinate', {
+        id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4 },
+        username: { type: Sequelize.STRING, allowNull: false },
+        latitude: Sequelize.DOUBLE,
+        longitude: Sequelize.DOUBLE
+    });
+    await coordinates.sync();
+    var created = await coordinates.create({username: username, latitude: latitude, longitude: longitude});
+    return true;
+}
+
+async function initialise() {
+    instance = initSequelize();
+    return await verifySequelize(instance);
+}
+
+export { initialise, saveCoordinates }
